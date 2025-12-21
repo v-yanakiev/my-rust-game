84053d07f856fbe3695613900f3f07d736653650
Vasil Yanakiev <vasil2000yanakiev@gmail.com>
2025-12-21 15:25:37 +0200

    game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,92 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+const MOVEMENT_SPEED: f32 = 200.0;
+const CIRCLE_RADIUS: f32 = 16.0;
+#[macroquad::main("My game")]
+async fn main() {
+    rand::srand(miniquad::date::now() as u64);
+    let mut squares = vec![];
+    let mut circle = Shape {
+        size: CIRCLE_RADIUS,
+        speed: MOVEMENT_SPEED,
+        x: screen_width() / 2.0,
+        y: screen_height() / 2.0,
+    };
+    let mut gameover = false;
+
+    loop {
+        if !gameover {
+            let delta_time = get_frame_time();
+            if is_key_down(KeyCode::Right) {
+                circle.x += MOVEMENT_SPEED * delta_time;
+            }
+            if is_key_down(KeyCode::Left) {
+                circle.x -= MOVEMENT_SPEED * delta_time;
+            }
+            if is_key_down(KeyCode::Up) {
+                circle.y -= MOVEMENT_SPEED * delta_time;
+            }
+            if is_key_down(KeyCode::Down) {
+                circle.y += MOVEMENT_SPEED * delta_time;
+            }
+            circle.x = clamp(circle.x, CIRCLE_RADIUS, screen_width() - CIRCLE_RADIUS);
+            circle.y = clamp(circle.y, CIRCLE_RADIUS, screen_height() - CIRCLE_RADIUS);
+
+            if rand::gen_range(0, 99) >= 95 {
+                let size = rand::gen_range(16.0, 64.0);
+                squares.push(Shape {
+                    size,
+                    speed: rand::gen_range(50.0, 150.0),
+                    x: rand::gen_range(size / 2.0, screen_width() - size / 2.0),
+                    y: -size,
+                })
+            }
+            for square in &mut squares {
+                square.y += square.speed * delta_time;
+            }
+            squares.retain(|square| square.y < screen_height() + square.size);
+        }
+        draw_circle(circle.x, circle.y, CIRCLE_RADIUS, YELLOW);
+
+        for square in &squares {
+            draw_rectangle(
+                square.x - square.size / 2.0,
+                square.y - square.size / 2.0,
+                square.size,
+                square.size,
+                GREEN,
+            );
+        }
+        if squares.iter().any(|square| circle.collides_with(square)) {
+            gameover = true;
+        }
+        if(gameover&&is_key_pressed(KeyCode::Space)){
+            squares.clear();
+            circle.x=screen_width() / 2.0;
+            circle.y=screen_height() / 2.0;
+            gameover=false;
+        }
+        next_frame().await;
+    }
+}
+
+struct Shape {
+    size: f32,
+    speed: f32,
+    x: f32,
+    y: f32,
+}
+
+impl Shape {
+    fn collides_with(&self, other: &Self) -> bool {
+        self.rect().overlaps(&other.rect())
+    }
+    fn rect(&self) -> Rect {
+        Rect {
+            x: self.x - self.size / 2.0,
+            y: self.y - self.size / 2.0,
+            w: self.size,
+            h: self.size,
+        }
+    }
 }
