689ff700cc3516893b4fd09d812e2d8cfefeaac2
Vasil Yanakiev <vasil2000yanakiev@gmail.com>
2025-12-21 15:59:08 +0200

    firing

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,10 @@ async fn main() {
         speed: MOVEMENT_SPEED,
         x: screen_width() / 2.0,
         y: screen_height() / 2.0,
+        collided: false,
     };
+    let mut bullets: Vec<Shape> = vec![];
+
     let mut gameover = false;
 
     loop {
@@ -39,12 +42,39 @@ async fn main() {
                     speed: rand::gen_range(200.0, 500.0),
                     x: rand::gen_range(size / 2.0, screen_width() - size / 2.0),
                     y: -size,
+                    collided: false,
                 })
             }
             for square in &mut squares {
                 square.y += square.speed * delta_time;
             }
             squares.retain(|square| square.y < screen_height() + square.size);
+
+            if is_key_pressed(KeyCode::Space) {
+                bullets.push(Shape {
+                    x: circle.x,
+                    y: circle.y,
+                    speed: circle.speed * 2.0,
+                    size: 5.0,
+                    collided: false,
+                })
+            }
+            for bullet in &mut bullets {
+                bullet.y -= bullet.speed * delta_time;
+            }
+            bullets.retain(|bullet| bullet.y > 0.0 - bullet.size / 2.0);
+
+            squares.retain(|square| !square.collided);
+            bullets.retain(|bullet| !bullet.collided);
+
+            for square in squares.iter_mut(){
+                for bullet in bullets.iter_mut(){
+                    if bullet.collides_with(square){
+                        bullet.collided=true;
+                        square.collided=true;
+                    }
+                }
+            }
         }
         draw_circle(circle.x, circle.y, CIRCLE_RADIUS, YELLOW);
 
@@ -56,6 +86,10 @@ async fn main() {
                 square.size,
                 GREEN,
             );
+        }
+
+        for bullet in &bullets {
+            draw_circle(bullet.x, bullet.y, bullet.size/2.0, RED);
         }
         if squares.iter().any(|square| circle.collides_with(square)) {
             gameover = true;
@@ -76,6 +110,7 @@ async fn main() {
             circle.y = screen_height() / 2.0;
             gameover = false;
         }
+
         next_frame().await;
     }
 }
@@ -85,6 +120,7 @@ struct Shape {
     speed: f32,
     x: f32,
     y: f32,
+    collided: bool,
 }
 
 impl Shape {
